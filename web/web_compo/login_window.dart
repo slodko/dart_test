999bd54690c112e82be2f3470b2ead3ec4eec73f
kolar <slodko@gmail.com>
2014-02-05 20:58:07 +0100

    init

---
+++ web/web_compo/login_window.dart
@@ -0,0 +1,11 @@
+library cultist_login_window;
+
+import 'package:polymer/polymer.dart';
+
+@CustomTag('login-window')
+class LoginWindow extends PolymerElement {
+
+  LoginWindow.created() : super.created() {
+  }
+
+}
